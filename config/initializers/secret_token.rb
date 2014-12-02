de1e3d6a6e8adb7a6ed9ab1b51b50a458f204db0
cerico <garethrobertlee@gmail.com>
2014-12-02 16:16:17 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pennine::Application.config.secret_token = 'e6c713d71901d11bf3ff054d57c0864ebbee9725635a1adeaa3eea60900ce1ff6b4d06ad30ede72ff1bac38cc8f263539fa1526af0df0d4922ca80c1d6549a89'
