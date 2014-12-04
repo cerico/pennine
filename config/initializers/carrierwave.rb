977d68cf6b466b3a9185578c6715ac5e4c500c61
cerico <garethrobertlee@gmail.com>
2014-12-04 01:07:57 +0000

    trying config.action_controller.asset_host = //BUCKETNAME.s3.amazonaws.com in application.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider  => 'AWS',  # required
     :aws_access_key_id  => ENV['aws_access_key_id'],  # required
     :aws_secret_access_key => ENV['aws_secret_access_key'],  # required
-
+    :region  => 'eu-west-1',  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'scenic'  # required
 
