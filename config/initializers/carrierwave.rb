39f3a0c44a9b1664ca40abbc47725a6cfd884aab
cerico <garethrobertlee@gmail.com>
2014-12-04 00:41:40 +0000

    added region to initializer for s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,8 @@ CarrierWave.configure do |config|
     :aws_secret_access_key => ENV['aws_secret_access_key'],  # required
     :region  => 'eu-west-1',  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = ENV['SCENICBUCKET']  # required
+  config.fog_directory  = 'scenicone'  # required
+  config.fog_host = 'https://s3-eu-west-1.amazonaws.com/scenicone'
   config.fog_public  = true  # optional, defaults to true
 end
 
