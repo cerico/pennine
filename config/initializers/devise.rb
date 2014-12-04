2054c23fe3fa6736f3e1e96acc460293f0a4d327
cerico <garethrobertlee@gmail.com>
2014-12-04 19:38:01 +0000

    google information added to devise.rb initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,4 +256,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :google_oauth2, ENV['GOOGLE_AUTH_CLIENT_ID_SCENIC'], ENV['GOOGLE_AUTH_CLIENT_SECRET_SCENIC'], scope: "email, profile", client_options:{ image_aspect_ratio: "square", image_size: 30 }
 end
