83ff92a80318e911db06b0fa8095446a00d042e9
cerico <garethrobertlee@gmail.com>
2014-12-07 18:46:21 +0000

    user controller modified with addeds, for profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @favs = @user.bookmarks.where(favourited:true)
     @completed = @user.bookmarks.where(completed:true)
+    @added = Trail.where(user_id:@user.id)
 
     respond_to do |format|
       format.html # show.html.erb
