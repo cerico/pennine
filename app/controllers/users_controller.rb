c9ebc3d8aff4fedc41670f8fe5d60690db21c649
cerico <garethrobertlee@gmail.com>
2014-12-07 18:45:44 +0000

    user controller modified with favs and completeds for profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @favs = @user.bookmarks.where(favourited:true)
+    @completed = @user.bookmarks.where(completed:true)
 
     respond_to do |format|
       format.html # show.html.erb
