c1e3a28b4833f65f038373799d908e6a60761872
cerico <garethrobertlee@gmail.com>
2014-12-07 18:47:54 +0000

    user controller modifed with @distance, and distance helper method created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :distance_calc
+
+  def distance_calc(completedhikes)
+    totaldistance = 0
+    completedhikes.each do |hike|
+      totaldistance += hike.trail.distance
+    end
+    totaldistance
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     @favs = @user.bookmarks.where(favourited:true)
     @completed = @user.bookmarks.where(completed:true)
     @added = Trail.where(user_id:@user.id)
+    @distance = view_context.distance_calc(@completed)
 
     respond_to do |format|
       format.html # show.html.erb
