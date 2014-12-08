8c1a1818d6c8fdf9fe0de48fd4c5f696f9873e4a
cerico <garethrobertlee@gmail.com>
2014-12-08 17:25:34 +0000

    added line to application.rb re:precompile

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,7 @@ module Pennine
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
     config.assets.initialize_on_precompile = false
     
   end
