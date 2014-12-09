25e8ffec3d067cb68355cf124bcacbe7c9465471
cerico <garethrobertlee@gmail.com>
2014-12-09 15:25:39 +0000

    added photos to serializer, and referenced photos via angular on trails#show page:

--- app/serializers/trail_serializer.rb
+++ app/serializers/trail_serializer.rb
@@ -2,6 +2,8 @@ class TrailSerializer < ActiveModel::Serializer
   attributes :id, :name, :county, :rating, :user_id, :distance, :lat, :lng, :userbookmark, :user
 
   has_many :bookmarks
+  has_many :photos
+  
 
   def userbookmark
     scope.bookmarks.find_by_trail_id(object) if scope
