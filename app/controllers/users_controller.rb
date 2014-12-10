4ee7ca1f2b0f5b66c80df57cc4b29a927ea38f43
cerico <garethrobertlee@gmail.com>
2014-12-10 13:17:09 +0000

    removed user serializer, and used as json instead for profile page, moved distance calc to a front end function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,14 @@ class UsersController < ApplicationController
     @completed = @user.bookmarks.where(completed:true)
     @added = Trail.where(user_id:@user.id)
     @distance = view_context.distance_calc(@completed)
-
+    
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @user,  root: false  }
+      # format.json { render json: @user, :include => {:bookmarks => {:include => { :trail => { :include => :photos } }} }), root: false }
+      format.json { render json: @user, :include => [{:bookmarks => {:include => { :trail => { :include => :photos } }} },:trails] }
+      
+
+
     end
   end
 end
