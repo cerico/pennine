4df245b732063cadbb7480365dad029f524d68ac
cerico <garethrobertlee@gmail.com>
2014-12-14 09:21:58 +0000

    replaced user with hiker for current_user

--- app/serializers/trail_serializer.rb
+++ app/serializers/trail_serializer.rb
@@ -1,15 +1,16 @@
 class TrailSerializer < ActiveModel::Serializer
-  attributes :id, :name, :county, :rating, :user_id, :distance, :lat, :lng, :userbookmark, :user
+  attributes :id, :name, :county, :rating, :user_id, :distance, :lat, :lng, :hikerbookmark, :hiker
 
   has_many :bookmarks
   has_many :photos
+  has_one :user
   
 
-  def userbookmark
+  def hikerbookmark
     scope.bookmarks.find_by_trail_id(object) if scope
   end
 
-  def user
+  def hiker
     scope
   end
 end
