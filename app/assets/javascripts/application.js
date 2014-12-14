6705afd7b035926fdba2fa387863db5ff9d96da8
cerico <garethrobertlee@gmail.com>
2014-12-14 08:23:26 +0000

    angular before jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
 //= require angular/angular
 //= require angular-ui-router/release/angular-ui-router
 //= require angular-rails-templates
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
